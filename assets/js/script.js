cf10918228eb17746ff4bd32b0949c6b2ba41965
Justinperry999 <>
2023-07-25 18:32:57 +0000

    Add functions for rungame, computerchoice, userwin, userloser and draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            let Choice = this.getAttribute("data-type");
-            let Weapon = document.getElementById("userschoice");
+            let userChoice = this.getAttribute("data-type");
+            runGame(userChoice);
 
-            runGame(Choice, Weapon);
         });
 
         //Add event listener to the start game button
@@ -35,7 +34,41 @@ document.addEventListener("DOMContentLoaded", function () {
     /**
  * Contain the main game functionality, called when the user select their first weapon
  */
-    function runGame(choice, weapon) {
+    function runGame(userChoice) {
+        let computerChoice = getComputerChoice();
+        switch (userChoice + computerChoice) {
+            case "scissorspaper":
+            case "paperrock":
+            case "rocklizard":
+            case "lizardspock":
+            case "spockscissors":
+            case "scissorslizard":
+            case "lizardpaper":
+            case "paperspock":
+            case "spockrock":
+            case "rockscissors":
+                userWin(userChoice,computerChoice);
+                break;
+            case "paperscissors":
+            case "rockpaper":
+            case "lizardrock":
+            case "spocklizard":
+            case "scissorsspock":
+            case "lizardscissors":
+            case "paperlizard":
+            case "spockpaper":
+            case "rockspock":
+            case "scissorsrock":
+                userLose(userChoice,computerChoice);
+                break;
+            case "paperpaper":
+            case "rockrock":
+            case "lizardlizard":
+            case "spockspock":
+            case "scissorsscissors":
+                Draw(userChoice,computerChoice);
+                break;
+        }
 
     }
 
@@ -44,17 +77,52 @@ document.addEventListener("DOMContentLoaded", function () {
     }
    
 
-    function compWeapon() {
+    function getComputerChoice() {
+        let choices = ['rock','paper','scissors','lizard','spock'];
+        let randomNumber = Math.floor(Math.random()*5);
+        return(choices[randomNumber]);
+    }
+
+    function userWin(userChoice,computerChoice) {
+        let userScore_span = document.getElementById("user-score");
+        let compScore_span = document.getElementById("comp-score");
+        let userScore = parseInt(userScore_span.innerText);
+        let compScore = parseInt(compScore_span.innerText);;
+        let roundResult_p = document.querySelector(".round-result > p");
+        let gameResult = document.getElementById("game-result");
+        userScore++;
+        userScore_span.innerHTML = userScore;
+        compScore_span.innerHTML = compScore;
+        if (userScore < 5) {
+        roundResult_p.innerHTML = `${userChoice} beats ${computerChoice} You Win!`;
+        } else if (userScore === 5){
+        gameResult.innerText = "You Reached 5! YOU WON!";
+    }; 
 
     }
 
-    function displayOutcome() {
-
+    function userLose(userChoice,computerChoice) {
+        let userScore_span = document.getElementById("user-score");
+        let compScore_span = document.getElementById("comp-score");
+        let userScore = parseInt(userScore_span.innerText);
+        let compScore = parseInt(compScore_span.innerText);;
+        let roundResult_p = document.querySelector(".round-result > p");
+        let gameResult = document.getElementById("game-result");
+        compScore++;
+        userScore_span.innerHTML = userScore;
+        compScore_span.innerHTML = compScore;
+        if (compScore < 5) {
+        roundResult_p.innerHTML = `${computerChoice} beats ${userChoice} You Lose!`;
+        } else if (compScore === 5){
+        gameResult.innerText = "Sorry, YOU LOST!";
+    }; 
     }
 
-    function incrementUserScore() {
-
+    function Draw (userChoice,computerChoice) {
+        let roundResult_p = document.querySelector(".round-result > p");
+         roundResult_p.innerHTML = `${userChoice} matches ${computerChoice} It's a Draw!`;
     }
+
 
     function incrementCompScore() {
 
