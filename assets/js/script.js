bcd7190278e3f3d83cef6421bab54127566d4b73
Justinperry999 <>
2023-07-26 16:47:48 +0000

    Add endGame, clearScores and restartGame functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,113 +25,132 @@ document.addEventListener("DOMContentLoaded", function () {
         //Add event listener to the play again button
         document.getElementById("play-again-btn").addEventListener("click", function () {
             document.getElementById("game-result").style.display = "none";
-            clearScore();
+            clearScores();
         });
     }
 });
-    // Main game functions
+// Main game functions
 
-    /**
- * Contain the main game functionality, called when the user select their first weapon
- */
-    function runGame(userChoice) {
-        let computerChoice = getComputerChoice();
-        switch (userChoice + computerChoice) {
-            case "scissorspaper":
-            case "paperrock":
-            case "rocklizard":
-            case "lizardspock":
-            case "spockscissors":
-            case "scissorslizard":
-            case "lizardpaper":
-            case "paperspock":
-            case "spockrock":
-            case "rockscissors":
-                userWin(userChoice,computerChoice);
-                break;
-            case "paperscissors":
-            case "rockpaper":
-            case "lizardrock":
-            case "spocklizard":
-            case "scissorsspock":
-            case "lizardscissors":
-            case "paperlizard":
-            case "spockpaper":
-            case "rockspock":
-            case "scissorsrock":
-                userLose(userChoice,computerChoice);
-                break;
-            case "paperpaper":
-            case "rockrock":
-            case "lizardlizard":
-            case "spockspock":
-            case "scissorsscissors":
-                Draw(userChoice,computerChoice);
-                break;
-        }
-
+/**
+* Contain the main game functionality, called when the user select their first weapon
+*/
+function runGame(userChoice) {
+    let computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "scissorspaper":
+        case "paperrock":
+        case "rocklizard":
+        case "lizardspock":
+        case "spockscissors":
+        case "scissorslizard":
+        case "lizardpaper":
+        case "paperspock":
+        case "spockrock":
+        case "rockscissors":
+            userWin(userChoice, computerChoice);
+            break;
+        case "paperscissors":
+        case "rockpaper":
+        case "lizardrock":
+        case "spocklizard":
+        case "scissorsspock":
+        case "lizardscissors":
+        case "paperlizard":
+        case "spockpaper":
+        case "rockspock":
+        case "scissorsrock":
+            userLose(userChoice, computerChoice);
+            break;
+        case "paperpaper":
+        case "rockrock":
+        case "lizardlizard":
+        case "spockspock":
+        case "scissorsscissors":
+            Draw(userChoice, computerChoice);
+            break;
     }
 
-    function userWeapon(choice, weapon) {
+}
 
-    }
-   
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    let randomNumber = Math.floor(Math.random() * 5);
+    return (choices[randomNumber]);
+}
 
-    function getComputerChoice() {
-        let choices = ['rock','paper','scissors','lizard','spock'];
-        let randomNumber = Math.floor(Math.random()*5);
-        return(choices[randomNumber]);
-    }
-
-    function userWin(userChoice,computerChoice) {
-        let userScore_span = document.getElementById("user-score");
-        let compScore_span = document.getElementById("comp-score");
-        let userScore = parseInt(userScore_span.innerText);
-        let compScore = parseInt(compScore_span.innerText);;
-        let roundResult_p = document.querySelector(".round-result > p");
-        let gameResult = document.getElementById("game-result");
-        userScore++;
-        userScore_span.innerHTML = userScore;
-        compScore_span.innerHTML = compScore;
-        if (userScore < 5) {
+function userWin(userChoice, computerChoice) {
+    let userScore_span = document.getElementById("user-score");
+    let compScore_span = document.getElementById("comp-score");
+    let userScore = parseInt(userScore_span.innerText);
+    let compScore = parseInt(compScore_span.innerText);;
+    let roundResult_p = document.querySelector(".round-result > p");
+    let gameResult = document.getElementById("game-result");
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    if (userScore < 5) {
         roundResult_p.innerHTML = `${userChoice} beats ${computerChoice} You Win!`;
-        } else if (userScore === 5){
+    } else if (userScore === 5) {
         gameResult.innerText = "You Reached 5! YOU WON!";
-    }; 
-
+        endGame();
     }
-
-    function userLose(userChoice,computerChoice) {
-        let userScore_span = document.getElementById("user-score");
-        let compScore_span = document.getElementById("comp-score");
-        let userScore = parseInt(userScore_span.innerText);
-        let compScore = parseInt(compScore_span.innerText);;
-        let roundResult_p = document.querySelector(".round-result > p");
-        let gameResult = document.getElementById("game-result");
-        compScore++;
-        userScore_span.innerHTML = userScore;
-        compScore_span.innerHTML = compScore;
-        if (compScore < 5) {
+}
+function userLose(userChoice, computerChoice) {
+    let userScore_span = document.getElementById("user-score");
+    let compScore_span = document.getElementById("comp-score");
+    let userScore = parseInt(userScore_span.innerText);
+    let compScore = parseInt(compScore_span.innerText);
+    let roundResult_p = document.querySelector(".round-result > p");
+    let gameResult = document.getElementById("game-result");
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    if (compScore < 5) {
         roundResult_p.innerHTML = `${computerChoice} beats ${userChoice} You Lose!`;
-        } else if (compScore === 5){
+    } else if (compScore === 5) {
         gameResult.innerText = "Sorry, YOU LOST!";
-    }; 
+        endGame();
     }
+}
+function Draw(userChoice, computerChoice) {
+    let roundResult_p = document.querySelector(".round-result > p");
+    roundResult_p.innerHTML = `${userChoice} equals ${computerChoice} It's a Draw!`;
+}
 
-    function Draw (userChoice,computerChoice) {
-        let roundResult_p = document.querySelector(".round-result > p");
-         roundResult_p.innerHTML = `${userChoice} matches ${computerChoice} It's a Draw!`;
-    }
+function endGame() {
+    let rock_div = document.getElementById("rock-btn");
+    let paper_div = document.getElementById("paper-btn");
+    let scissors_div = document.getElementById("scissors-btn");
+    let lizard_div = document.getElementById("lizard-btn");
+    let spock_div = document.getElementById("spock-btn");
+    rock_div.disabled = true;
+    paper_div.disabled = true;
+    scissors_div.disabled = true;
+    lizard_div.disabled = true;
+    spock_div.disabled = true;
+}
 
+function clearScores() {
+    let userScore_span = document.getElementById("user-score");
+    let compScore_span = document.getElementById("comp-score");
+    let userScore = parseInt(userScore_span.innerText);
+    let compScore = parseInt(compScore_span.innerText);
+    userScore = 0;
+    compScore = 0;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+}
 
-    function incrementCompScore() {
-
-    }
-
-    function countRounds() {
-
-    }
-
-    function clearScores() {
-
-    };
+function restartGame() {
+    clearScores();
+    let rock_div = document.getElementById("rock-btn");
+    let paper_div = document.getElementById("paper-btn");
+    let scissors_div = document.getElementById("scissors-btn");
+    let lizard_div = document.getElementById("lizard-btn");
+    let spock_div = document.getElementById("spock-btn");
+    rock_div.disabled = false;
+    paper_div.disabled = false;
+    scissors_div.disabled = false;
+    lizard_div.disabled = false;
+    spock_div.disabled = false;
+}
